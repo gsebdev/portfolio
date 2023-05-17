49a916186437a517b481d88507714abc523c1b05
Sebastien Gault <sgault.webdev@gmail.com>
2023-05-17 18:01:31 +0200

    added 2 custom hooks to handle events like keypres and click

---
+++ src/hooks/useKeyPressByRef.tsx
@@ -0,0 +1,22 @@
+import { useCallback, useEffect } from "react";
+
+export default function useKeyPressByRef(ref: any, callback: () => void, keys: string[]) {
+    const element = ref.current
+    const handler = useCallback(({ code }: KeyboardEvent) => {
+        console.log(code)
+        if(keys.includes(code)) {
+            callback()
+        }
+    }, [callback, keys])
+
+    useEffect(() => {
+        if(element) {
+            element.addEventListener('keydown', handler)
+        }
+        return () => {
+            if(element){
+                element.removeEventListener('keydown', handler)
+            }
+        }
+    }, [element, handler, callback, keys])
+}
