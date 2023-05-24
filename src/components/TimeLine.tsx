2c0ded80df2d6f30889a44aa1aa6fa435fbe61ef
Sebastien Gault <sgault.webdev@gmail.com>
2023-05-24 16:00:52 +0200

    finished timeline component

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -1,8 +1,6 @@
-'use client'
-
 import { useMemo } from 'react'
 import styles from './timeline.module.scss'
-
+import { useState } from 'react'
 
 interface TimeLineElement {
     color?: string | undefined
@@ -19,27 +17,28 @@ interface TimeLineProps {
 const TimeLine: React.FC<TimeLineProps> = ({ timelineElements }) => {
 
     const elementWidthPercent = useMemo(() => 100 / timelineElements.length, [timelineElements])
+
     return (
         <ul className={styles.timeline}>
             {
                 timelineElements.map(({ color = 'white', title, content, time }, index) => {
+
                     const reverse = index % 2 === 0 ? true : false
+
                     return (
-                        <li key={index + title + 'timeline123456789'}
+                        <li 
+                            key={index + title + 'timeline123456789'}
                             className={`${styles.element} ${reverse ? styles.reverse : ''}`}
-                            style={{ width: `${elementWidthPercent}%` }}
+                            style={{ 
+                                width: `${elementWidthPercent}%`,
+                                opacity: `${1 - ((timelineElements.length - index) * (0.6 / timelineElements.length))}`
+                            }}
                         >
                             <div className={styles.text}>
-
-                                <div>
-                                    <h3 className={styles.title}>{title}</h3>
-                                    <p className={styles.content}>{content}</p>
-                                </div>
+                                <h3 className={styles.title}>{title}</h3>
                                 <p className={styles.time}>{time}</p>
-
                             </div>
-
-
+                            <p className={styles.content}>{content}</p>
                             <div className={styles.arrow} style={{ background: color }}></div>
                             <span className={styles.line}></span>
                         </li>
