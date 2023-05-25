f682c31c87e36e33236168d27135408b667c030d
Sebastien Gault <sgault.webdev@gmail.com>
2023-05-25 10:04:01 +0200

    some enhancements on timeline component

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from 'react'
+'use client'
+import { useCallback, useMemo } from 'react'
 import styles from './timeline.module.scss'
 import { useState } from 'react'
 
@@ -15,24 +16,36 @@ interface TimeLineProps {
 }
 
 const TimeLine: React.FC<TimeLineProps> = ({ timelineElements }) => {
+    const [activeElement, setActiveElement] = useState<number | null>(null)
 
+    //calc element width function of length of elements
     const elementWidthPercent = useMemo(() => 100 / timelineElements.length, [timelineElements])
 
+    // function to get the opacity value function of index position and legnth to make a gradient
+    const getOpacity = useCallback((index: number) => 1 - ((timelineElements.length - index) * (0.6 / timelineElements.length)), [timelineElements])
+
     return (
-        <ul className={styles.timeline}>
+        <ul
+            className={styles.timeline}
+            onBlur={() => { setActiveElement(null) }}
+            onMouseLeave={() => { setActiveElement(null) }}
+        >
             {
                 timelineElements.map(({ color = 'white', title, content, time }, index) => {
 
                     const reverse = index % 2 === 0 ? true : false
 
                     return (
-                        <li 
+                        <li
+                            tabIndex={0}
                             key={index + title + 'timeline123456789'}
-                            className={`${styles.element} ${reverse ? styles.reverse : ''}`}
-                            style={{ 
+                            className={`${styles.element} ${reverse ? styles.reverse : ''} ${activeElement === index ? styles.active : ''}`}
+                            style={{
                                 width: `${elementWidthPercent}%`,
-                                opacity: `${1 - ((timelineElements.length - index) * (0.6 / timelineElements.length))}`
+                                opacity: `${activeElement === null ? getOpacity(index) : activeElement === index ? '1' : '0.2'}`
                             }}
+                            onMouseOver={() => { setActiveElement(index) }}
+                            onFocus={() => { setActiveElement(index) }}
                         >
                             <div className={styles.text}>
                                 <h3 className={styles.title}>{title}</h3>
