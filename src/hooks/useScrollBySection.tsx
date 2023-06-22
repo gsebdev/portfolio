b9f3c7ad554c175324eca4a846128211e2de5caa
Sebastien Gault <sgault.webdev@gmail.com>
2023-06-22 16:16:15 +0200

    finished custom hook useScrollBySection

--- src/hooks/useScrollBySection.tsx
+++ src/hooks/useScrollBySection.tsx
@@ -1,134 +1,215 @@
 import { useCallback, useEffect, useRef, useState } from "react";
+interface useScrollBySectionReturnProps {
+    activeSection: number,
+    setActiveSection: (newActiveSection: number) => void
+}
+interface wheelRunRefProps {
+    lastEvent: WheelEvent | null
+    maxDeltaValue: number
+    timeout: NodeJS.Timeout | null
+    wheeling: boolean
+}
 
-export default function useScrollBySection(container: React.RefObject<HTMLElement>, transitionDuration: number, hideScrollBar: true | false = true) {
+const useScrollBySection = (container: React.RefObject<HTMLElement>, transitionDuration: number, hideScrollBar: boolean = true, autoScroll: boolean = true): useScrollBySectionReturnProps => {
     const [activeSection, setActiveSection] = useState<number>(0)
     const [sections, setSections] = useState<HTMLElement[]>([])
-    const isTransition = useRef(false)
+    const activeSectionRef = useRef<HTMLElement | null>(null)
+    const isTransition = useRef<boolean>(false)
+    const wheelRun = useRef<wheelRunRefProps>({
+        lastEvent: null,
+        maxDeltaValue: 0,
+        timeout: null,
+        wheeling: false
+    })
 
-    //hide body scrollbar if `hideScrollBar`is true
-    useEffect(() => {
-        if(hideScrollBar) {
-            document.body.setAttribute('style', 'overflow: hidden')
-        }
-        return () => {
-            document.body.removeAttribute('style')
-        }
-    }, [hideScrollBar])
+    //Callback function to animate the scroll to the active section
+    const animateScrollTo = useCallback((scrollTarget: number) => {
+        let startTime: number = 0
+        const scrollStart: number = window.scrollY
 
-    // get an array of the sections in the container
+        const easeInOutQuart = (x: number): number => {
+            return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+        }
+
+        const animateScroll = (timeStamp: number) => {
+            if (!startTime) startTime = Math.round(timeStamp)
+
+            const time: number = Math.min(((Math.round(timeStamp) - startTime) / transitionDuration), 1)
+
+            const position: number = easeInOutQuart(time)
+
+            if (position < 1) {
+                window.scroll(0, scrollStart + (scrollTarget - scrollStart) * position)
+                requestAnimationFrame(animateScroll)
+            } else {
+                window.scroll(0, scrollTarget)
+                isTransition.current = false
+            }
+        }
+        if ((scrollTarget - scrollStart) !== 0) {
+            isTransition.current = true
+            requestAnimationFrame(animateScroll)
+        }
+    }, [transitionDuration])
+
+    //test if section is into viewport
+    const isSectionIntoView = useCallback((section: HTMLElement) => {
+        const { top, bottom } = section.getBoundingClientRect()
+                    if ((top < 0 && bottom <= 0) || (top >= window.innerHeight)) {  // the section is upper the top of the viewport or is under the viewport
+                        return false
+                    } else {
+                        return (top < (window.innerHeight / 2)) && (bottom > (window.innerHeight / 2)) ? true : false // if the section is more than 50% in the viewwport
+                    }
+    }, [])
+
     useEffect(() => {
         if (container.current) {
-            const sections = container.current.children as HTMLCollectionOf<HTMLElement>
-            setSections(Array.from(sections))
+            setSections(Array.from(container.current.children as HTMLCollectionOf<HTMLElement>))
         }
     }, [container])
 
-    //animate scroll to the active section
+    // effect to scroll to the active section
     useEffect(() => {
+        activeSectionRef.current = sections[activeSection]
+        if (activeSectionRef.current && !isSectionIntoView(activeSectionRef.current)) animateScrollTo(activeSectionRef.current.offsetTop)
+    }, [activeSection, sections, animateScrollTo, isSectionIntoView])
 
-        if (sections[activeSection]) {
-            let startTime: number = 0
-            const scrollTarget: number = sections[activeSection].offsetTop
-            const scrollStart: number = window.scrollY
-            const easeInOutQuart = (x: number): number => {
-                return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
-            }
-            const animateScroll = (timeStamp: number) => {
-                if (!startTime) startTime = Math.round(timeStamp)
-
-                const time: number = Math.min(((Math.round(timeStamp) - startTime) / transitionDuration), 1)
-
-                const position: number = easeInOutQuart(time)
-
-                if (position < 1) {
-                    window.scroll(0, scrollStart + (scrollTarget - scrollStart) * position)
-                    requestAnimationFrame(animateScroll)
-                } else {
-                    window.scroll(0, scrollTarget)
-                    isTransition.current = false
+    //hide body scrollbar if `hideScrollBar`is true
+    useEffect(() => {
+        const style = document.createElement('style')
+        if (hideScrollBar) {
+            style.innerHTML = `
+                body::-webkit-scrollbar {
+                    display: none;
+                };
+                body {
+                    -ms-overflow-style: none;
+                    scrollbar-width: none; 
                 }
-            }
-            if ((scrollTarget - scrollStart) !== 0) {
-                isTransition.current = true
-                requestAnimationFrame(animateScroll)
-            }
-
+            `
+            const head = document.querySelector('head')
+            head?.appendChild(style)
         }
+        return () => {
+            style.remove()
+        }
+    }, [hideScrollBar])
 
-    }, [activeSection, sections, transitionDuration])
-
-    const goToNextSection = useCallback(() => {
-        const isSectionBottomVisible = (section: HTMLElement) => {
+    const scrollNext = useCallback(() => {
+        const section: HTMLElement | null = activeSectionRef.current
+        if (section) {
             const { bottom } = section.getBoundingClientRect()
-            return bottom <= window.innerHeight ? true : false
-        } // checks if the bottom of the current section is visible
-        setActiveSection(a => isSectionBottomVisible(sections[a]) ? Math.min(sections.length - 1, a + 1) : a)
-    }, [sections])
+            //if bottom of the section is visible
+            if (bottom <= window.innerHeight) {
+                setActiveSection(a => Math.min(sections.length - 1, a + 1))
+            } else {
+                const scrollTarget = window.scrollY + Math.max(window.innerHeight * 0.67, bottom)
+                animateScrollTo(scrollTarget)
+            }
+        }
+    }, [animateScrollTo, sections])
 
-    const goToPreviousSection = useCallback(() => {
-        const isSectionTopVisible = (section: HTMLElement) => {
+    const scrollPrev = useCallback(() => {
+        const section: HTMLElement | null = activeSectionRef.current
+        if (section) {
             const { top } = section.getBoundingClientRect()
-            return top >= 0 ? true : false
-        } // checks if the top of the current section is visible
-        setActiveSection(a => isSectionTopVisible(sections[a]) ? Math.max(0, a - 1) : a)
-    }, [sections])
+            //if top of the section is visible
+            if (top >= 0) {
+                setActiveSection(a => Math.max(0, a - 1))
+            } else {
+                const scrollTarget = window.scrollY - Math.max(Math.abs(top), window.innerHeight * 0.67)
+                animateScrollTo(scrollTarget)
+            }
+        }
+    }, [animateScrollTo])
 
     //listen for mouse wheel and handle the change of active section
     useEffect(() => {
-
         // Wheel event handler
         const handleWheel = (e: WheelEvent) => {
-            if (isTransition.current) e.preventDefault()
-            if (!isTransition.current) {
+            e.preventDefault()
+            const resetWheelRun = () => {
+                wheelRun.current = {
+                    lastEvent: null,
+                    maxDeltaValue: 0,
+                    timeout: null,
+                    wheeling: false
+                }
+            }
+            const wRun = wheelRun.current
+            // starts a new wheel run record
+            if (!wRun.wheeling) {
+                wRun.maxDeltaValue = Math.abs(e.deltaY)
+                wRun.timeout = setTimeout(() => { resetWheelRun() }, 250)
+                wRun.wheeling = true
+            } else {
+                //update the current wheel run record
+                if (wRun.timeout !== null) clearTimeout(wRun.timeout)
+                wRun.maxDeltaValue = Math.max(wRun.maxDeltaValue, Math.abs(e.deltaY))
+                wRun.timeout = setTimeout(() => { resetWheelRun() }, 250)
+            }
+            const isDirectionChanging: boolean = !wRun.lastEvent || Math.sign(wRun.lastEvent.deltaY) !== Math.sign(e.deltaY)
+            const isWheelingFast: boolean = Math.abs(e.deltaY) >= wRun.maxDeltaValue / 3
+            
+            if (!isTransition.current && (isDirectionChanging || isWheelingFast)) {
                 switch (Math.sign(e.deltaY)) {
                     case -1:
-                        goToPreviousSection()
+                        scrollPrev()
                         break
                     case 1:
-                        goToNextSection()
+                        scrollNext()
                         break
                 }
             }
+            wRun.lastEvent = e
         }
+
         const handleKeydown = (e: KeyboardEvent) => {
             const downKeys = ['Down', 'SpaceBar', ' ', 'ArrowDown', 'PageDown']
             const upKeys = ['Up', 'ArrowUp', 'PageUp']
 
-            if (downKeys.includes(e.key)) {
-                goToNextSection()
-            }
-            if (upKeys.includes(e.key)) {
-                goToPreviousSection()
-            }
+            if (downKeys.includes(e.key)) scrollNext()
+            if (upKeys.includes(e.key)) scrollPrev()
 
         }
-        const handleScroll = (e: Event) => {
-            if(!isTransition.current) {
+
+        const handleScroll = () => {
+            //spy active section
+            if (!isTransition.current) {
                 const sectionIntoView = sections.findIndex(section => {
-                    const {top, bottom} = section.getBoundingClientRect()
-                    if((top < 0 && bottom <= 0) || (top >= window.innerHeight)) {  // the section is upper the top of the viewport or is under the viewport
-                        return false
-                    }else {
-                        return (top < (window.innerHeight / 2)) && (bottom > (window.innerHeight / 2)) ? true : false // if the section is more than 50% in the viewwport
-                    }
+                    return isSectionIntoView(section)
                 })
-                if (sectionIntoView !== -1) setActiveSection(a => a !== sectionIntoView ? sectionIntoView : a) // if found an index setActiveSection only if activeSection changed
-                
+                if (sectionIntoView !== -1) setActiveSection(a => a !== sectionIntoView ? sectionIntoView : a) // if found an index setActiveSection only if activeSection changed    
             }
-
         }
-        
+
+        const handleWindowResize = () => {
+            isTransition.current = true
+            if (activeSectionRef.current) window.scroll(0, activeSectionRef.current.offsetTop)
+            isTransition.current = false
+        }
+
         // Add the event listener
-        document.addEventListener('wheel', handleWheel, { passive: false })
-        document.addEventListener('keydown', handleKeydown)
+        if (autoScroll) {
+            document.addEventListener('keydown', handleKeydown)
+            document.addEventListener('wheel', handleWheel, { passive: false })
+        }
         document.addEventListener('scroll', handleScroll)
+        window.addEventListener('resize', handleWindowResize)
         // When unmouts remove the event listener
         return () => {
-            document.removeEventListener('wheel', handleWheel), { passive: false }
+            if (autoScroll) {
+                document.removeEventListener('keydown', handleKeydown)
+                document.removeEventListener('wheel', handleWheel)
+
+            }
             document.removeEventListener('keydown', handleKeydown)
             document.removeEventListener('scroll', handleScroll)
+            window.removeEventListener('resize', handleWindowResize)
         }
-    }, [sections, goToNextSection, goToPreviousSection])
+    }, [autoScroll, scrollNext, scrollPrev, sections, isSectionIntoView])
 
     return { activeSection, setActiveSection }
 }
+
+export default useScrollBySection
